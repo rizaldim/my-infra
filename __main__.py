ef46862d3b98326ba905b36657b1f87e5a3d6513
Rizaldi Martaputra <zal@fastmail.com>
2024-02-03 16:04:55 +0700

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,16 @@
+import pulumi
+import pulumi_civo as civo
+
+region = "LON1"
+
+obj_store_cred = civo.ObjectStoreCredential("my-obj-store-cred", region=region)
+
+obj_store = civo.ObjectStore("pulumi-state",
+                         	access_key_id=obj_store_cred.access_key_id,
+                         	max_size_gb=500,
+                         	region=region)
+
+pulumi.export('obj_store_cred_access_key', obj_store_cred.access_key_id)
+pulumi.export('obj_store_url', obj_store.bucket_url)
+pulumi.export('obj_store_name', obj_store.name)
+
